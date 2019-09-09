a92af1d628b59a34a05cfa19c18abc6bc5d0e425
Alexandros Leontakianakos <alleon20@gmail.com>
2019-09-09 13:22:49 +0300

    Update authentication route

--- routes/post.admin.authenticate.js
+++ routes/post.admin.authenticate.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const hashUser = require('../helpers/hashUser');
 
 router.post('/admin/authenticate', require('../helpers/routeWrapper')(async (req) => {
-	if (!req.body.email || !req.body.password) {
-		throw new Error('Email or password not given');
+	if (!req.body.username || !req.body.password) {
+		throw new Error('Username or password not given');
 	}
 
-	const user = `${req.body.email}:${req.body.password}`;
+	const user = `${req.body.username}:${req.body.password}`;
 	const hash = hashUser(user);
 
 	if (req.app.get('admins').includes(hash)) {
 		return hash;
 	} else {
-		throw new Error('Invalid email or password');
+		throw new Error('Invalid username or password');
 	}
 }));
 
